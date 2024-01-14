b6f30ae7686a656a46739c7bd6e0011cc281b905
mayarrrrrr <akokmayar607@gmail.com>
2024-01-14 12:58:20 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  if (array.length === 0 && typeof array !== "number"){
+    return "input cannot be empty and write numbers only"
+  }
+
+  for (let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      if (array[i]+array[j] === target){
+        return true;
+      }
+    }
+  }
+  return false
 }
 
+let result = hasTargetSum([10,5,8,4],20)
+console.log(result)
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  create a function hasTargetSum
+  pass an array and the target sum as arguements
+  iterate through the array to see if a set on addition equals target sum
+  if two sets return the target sum return true
+  if not return false
 */
 
 /*
@@ -31,4 +52,4 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
-module.exports = hasTargetSum;
+module.export = hasTargetSum;
